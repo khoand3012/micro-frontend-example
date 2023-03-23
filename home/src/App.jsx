0ebc1b49a173926ee1964b4909cabf1732f5a3bb
Khoa Nguyen <aaronfebruary95@gmail.com>
2023-03-23 23:09:51 +0700

    Add load remote

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import Header from "./Header";
+// import Header from "./Header";
+const Header = React.lazy(() => loadRemoteModule('header_v1', './Header'))
 import Footer from "./Footer";
+import { loadRemoteModule } from "./load-remote-modules";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
--- home/src/index.js
+++ home/src/index.js
@@ -1,1 +1,7 @@
-import("./App");
+import { setRemoteDefinitions } from "./load-remote-modules";
+import("./bootstrap");
+
+fetch("./remote-modules.manifest.json")
+  .then((res) => res.json())
+  .then((definitions) => setRemoteDefinitions(definitions))
+  .then(() => import("./bootstrap").catch((err) => console.error(err)));
